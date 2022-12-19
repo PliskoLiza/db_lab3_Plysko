506afe604077e10a6d620d846c9a215a310d23d8
PliskoLiza <72132892+PliskoLiza@users.noreply.github.com>
2022-12-19 21:14:50 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,17 +1,3 @@
-SELECT * FROM genres
-
-SELECT * FROM film
- 
-SELECT * FROM filmgenres
-
-SELECT * FROM filmrating
-
-SELECT * FROM filmawards
-
---SELECT * FROM awards
---SELECT * FROM rating_platforms
-
-
 INSERT INTO
 Rating_platforms(platform_id, platform_name)
 VALUES
